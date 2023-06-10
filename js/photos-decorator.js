1de000652d217e5891aeafaea0250c67cb9a786b
maar-ten <maar-ten@users.noreply.github.com>
2023-06-10 20:00:39 +0000

    add photo decorator script

---
+++ js/photos-decorator.js
@@ -0,0 +1,16 @@
+$.getJSON('photos.json', (data) => {
+    $('#images').html('');
+    $.each(data, (_, { url, title }) => {
+        const imageBox = $('<div/>').addClass('imageBox');
+        const anchor = $('<a/>').attr('data-fancybox', 'gallery').attr('data-caption', title).attr('href', url);
+        const img = $('<img/>').attr('src', url).attr('alt', '');
+
+        anchor.append(img);
+        imageBox.append(anchor).append(' ');
+        imageBox.appendTo('#images');
+    });
+});
+
+Fancybox.bind('[data-fancybox]', {
+    wheel: 'pan'
+});
