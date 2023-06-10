4dab41e188c54f806251b997b2655441a74eec3e
maar-ten <maar-ten@users.noreply.github.com>
2023-06-10 20:04:51 +0000

    run script when document has finished loading

--- js/photos-decorator.js
+++ js/photos-decorator.js
@@ -1,16 +1,18 @@
-$.getJSON('photos.json', (data) => {
-    $('#images').html('');
-    $.each(data, (_, { url, title }) => {
-        const imageBox = $('<div/>').addClass('imageBox');
-        const anchor = $('<a/>').attr('data-fancybox', 'gallery').attr('data-caption', title).attr('href', url);
-        const img = $('<img/>').attr('src', url).attr('alt', '');
+window.addEventListener('load', () => {
+    $.getJSON('photos.json', (data) => {
+        $('#images').html('');
+        $.each(data, (_, { url, title }) => {
+            const imageBox = $('<div/>').addClass('imageBox');
+            const anchor = $('<a/>').attr('data-fancybox', 'gallery').attr('data-caption', title).attr('href', url);
+            const img = $('<img/>').attr('src', url).attr('alt', '');
 
-        anchor.append(img);
-        imageBox.append(anchor).append(' ');
-        imageBox.appendTo('#images');
+            anchor.append(img);
+            imageBox.append(anchor).append(' ');
+            imageBox.appendTo('#images');
+        });
     });
-});
 
-Fancybox.bind('[data-fancybox]', {
-    wheel: 'pan'
+    Fancybox.bind('[data-fancybox]', {
+        wheel: 'pan'
+    });
 });
